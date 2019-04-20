5f1f12826fe81a21100ba0f0bc2e4c631b25bdfd
256hax <256hax@git>
2019-04-20 18:12:15 +0900

    feat: [Security] remove WordPress and Plugins version display.

--- home/kusanagi/blog/DocumentRoot/wp-content/themes/twentyfifteen-child/functions.php
+++ home/kusanagi/blog/DocumentRoot/wp-content/themes/twentyfifteen-child/functions.php
@@ -5,4 +5,17 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style')
   );
 }
+
+/*--- [Security] remove WordPress and Plugins version display ---*/
+// something like that => <meta name="generator" content="WordPress 5.1.1" />
+remove_action('wp_head','wp_generator'); // meta name="generator"
+
+// something like that => xxx.js?ver=4.6.1'
+function remove_cssjs_ver2( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+	$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver2', 9999 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver2', 9999 );
 ?>
--- home/kusanagi/keyboards/DocumentRoot/wp-content/themes/twentyseventeen-child/functions.php
+++ home/kusanagi/keyboards/DocumentRoot/wp-content/themes/twentyseventeen-child/functions.php
@@ -14,4 +14,17 @@ add_post_type_support( 'reviews', 'wpcom-markdown' );
 add_post_type_support( 'blog', 'wpcom-markdown' );
 add_post_type_support( 'photos', 'wpcom-markdown' );
 add_post_type_support( 'customize', 'wpcom-markdown' );
+
+/*--- [Security] remove WordPress and Plugins version display ---*/
+// something like that => <meta name="generator" content="WordPress 5.1.1" />
+remove_action('wp_head','wp_generator'); // meta name="generator"
+
+// something like that => xxx.js?ver=4.6.1'
+function remove_cssjs_ver2( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+	$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver2', 9999 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver2', 9999 );
 ?>
