502ba2e6b3d3142086393f3e5bcc106e4d17044e
256hax <256hax@git>
2019-05-11 12:07:18 +0900

    style: remove content at index(home page)

--- home/kusanagi/blog/DocumentRoot/wp-content/themes/twentyseventeen-child/template-parts/post/content.php
+++ home/kusanagi/blog/DocumentRoot/wp-content/themes/twentyseventeen-child/template-parts/post/content.php
@@ -49,27 +49,6 @@
 		</div><!-- .post-thumbnail -->
 	<?php endif; ?>
 
-	<div class="entry-content">
-		<?php
-		/* translators: %s: Name of current post */
-		the_content(
-			sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-				get_the_title()
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
-				'after'       => '</div>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
 	<?php
 	if ( is_single() ) {
 		twentyseventeen_entry_footer();
