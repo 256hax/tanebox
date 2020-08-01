cafb7835d57a57e2185827d1fffbd66033d4193d
256hax <256hax@git>
2020-08-01 15:21:22 +0900

    style: change layout to front-page(TOP page)

--- home/kusanagi/keyboards/DocumentRoot/wp-content/themes/twentyseventeen-child/templates/default-loop.php
+++ home/kusanagi/keyboards/DocumentRoot/wp-content/themes/twentyseventeen-child/templates/default-loop.php
@@ -6,23 +6,10 @@
 
 			<div id="su-post-<?php the_ID(); ?>" class="su-post">
 
-				<?php if ( has_post_thumbnail( get_the_ID() ) ) : ?>
-					<a class="su-post-thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-				<?php endif; ?>
-
-				<h2 class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
 				<div class="su-post-meta">
-					<?php _e( 'Posted', 'shortcodes-ultimate' ); ?>: <?php the_time( get_option( 'date_format' ) ); ?>
+					<?php the_time( get_option( 'date_format' ) ); ?>
+					<h2 class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				</div>
-
-				<div class="su-post-excerpt">
-					<?php the_excerpt(); ?>
-				</div>
-
-				<?php if ( have_comments() || comments_open() ) : ?>
-					<a href="<?php comments_link(); ?>" class="su-post-comments-link"><?php comments_number( __( '0 comments', 'shortcodes-ultimate' ), __( '1 comment', 'shortcodes-ultimate' ), '% comments' ); ?></a>
-				<?php endif; ?>
 
 			</div>
 
